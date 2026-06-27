d5d31b5753e69505087c0563c8a3d2f6766244fb
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-06-27 17:48:10 +0530

    Add solution: 0. median of two sorted arrays (java)

--- 0-median-of-two-sorted-arrays/0-median-of-two-sorted-arrays.java
+++ 0-median-of-two-sorted-arrays/0-median-of-two-sorted-arrays.java
@@ -1,7 +1,11 @@
 
-                    return (Math.max(maxLeftX, 
-                    maxLeftY) + Math.min(minRightX, 
-                    minRightY)) / 2.0;
+            int minRightY = (partitionY == y) ? Integer.MAX_VALUE : nums2[partitionY];
+
+            // Check if we found the correct partition
+            if (maxLeftX <= minRightY && maxLeftY <= minRightX) {
+                // Even total length
+                if ((x + y) % 2 == 0) {
+                    return (Math.max(maxLeftX, maxLeftY) + Math.min(minRightX, minRightY)) / 2.0;
                 } 
                 // Odd total length
                 else {
