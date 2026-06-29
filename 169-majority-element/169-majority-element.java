b5df76505ab286d035e4ee261eca0b524d5af597
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-06-29 12:15:59 +0530

    solution: 169. Majority Element (java)

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,8 +1,13 @@
 
 class Solution {
-    public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
-
+    public int majorityElement(int[] arr) { //o(n) o(1)
+        int count = 0;
+        int can = 0;
+        for(int num : arr){
+            if(count == 0) can = num;
+            if(num == can) count++;
+            else count--;
+        }
+        return can;
     }
 }
