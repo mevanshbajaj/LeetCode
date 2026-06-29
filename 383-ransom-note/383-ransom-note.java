47d79b74f79e4d265dcbaf635a4aab5f43df1195
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-06-29 15:49:09 +0530

    solution: 383. Ransom Note (java)

--- 383-ransom-note/383-ransom-note.java
+++ 383-ransom-note/383-ransom-note.java
@@ -1,12 +1,13 @@
-class Solution {
-    public boolean canConstruct(String ransomNote, String magazine) {
-        int[] freq = new int[26];
-        for(char ch : magazine.toCharArray()) freq[ch - 'a']++;
-        for(char ch : ransomNote.toCharArray()){ 
-        if(freq[ch - 'a'] == 0)
-        return false;
-        freq[ch - 'a']--;
-        }
-     return true;
-    }
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for(char ch : magazine.toCharArray()) freq[ch - 'a']++;
+        for(char ch : ransomNote.toCharArray()){ 
+        if(freq[ch - 'a'] == 0)
+        return false;
+        freq[ch - 'a']--;
+        }
+     return true;
+    }
 }
