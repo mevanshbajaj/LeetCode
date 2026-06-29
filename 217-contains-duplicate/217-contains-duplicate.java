3276a45556bafec687b1c409388421f7f7c515f8
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-06-29 12:27:09 +0530

    solution: 217. Contains Duplicate (java)

---
+++ 217-contains-duplicate/217-contains-duplicate.java
@@ -0,0 +1,10 @@
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) { set.add(num);
+        if(!set.contains(num)) return false;
+        }
+        else return true;
+    }
+}
