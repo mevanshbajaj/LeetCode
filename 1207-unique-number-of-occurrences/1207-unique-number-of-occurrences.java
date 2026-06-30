d19a37bb58f251adfefea20de2e84296e7811dc2
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-06-30 16:19:58 +0530

    solution: 1207. Unique Number of Occurrences (java)

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -1,7 +1,12 @@
 
 class Solution {
     public boolean uniqueOccurrences(int[] arr) {
-        Set<Integer,Integer> map = new Set<>();
-        for(int num:)
+        Map<Integer, Integer> map = new HashMap<>();
+        // count frequency
+        for (int num : arr)
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        // check uniqueness
+        HashSet<Integer> set = new HashSet<>(map.values());
+        return set.size() == map.size();
     }
 }
