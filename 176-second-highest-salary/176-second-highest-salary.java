6da64d39b48d12ba6030aacf8b817b037d791822
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-01 20:54:25 +0530

    solution: 176. Second Highest Salary (java)

--- 176-second-highest-salary/176-second-highest-salary.java
+++ 176-second-highest-salary/176-second-highest-salary.java
@@ -1,2 +1,4 @@
-# Write your MySQL query statement below
-select salary from employee where < (select max(salary) from employee);
+
+# Write your MySQL query statement below
+select max(salary) as SecondHighestSalary from employee where salary < (select max(salary) from 
+employee);
