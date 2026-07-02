258cda74a636bcca37917a83ff345e4678867af9
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-02 11:20:30 +0530

    solution: 197. Rising Temperature (java)

---
+++ 197-rising-temperature/197-rising-temperature.java
@@ -0,0 +1,5 @@
+
+# Write your MySQL query statement below
+select w1.id from weather w1 join weather w2
+ON w1.recordDate = DATE_ADD(w2.recordDate, INTERVAL 1 DAY) 
+where w1.temperature > w2.temperature
