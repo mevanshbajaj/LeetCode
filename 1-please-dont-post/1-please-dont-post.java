93cc51582a2afe70a8cc1df6e1b445b8371bd23d
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-03 16:27:01 +0530

    solution: 1. Please don't post (java)

--- 1-please-dont-post/1-please-dont-post.java
+++ 1-please-dont-post/1-please-dont-post.java
@@ -1,6 +1,8 @@
-class Solution {
-    public int threeSumClosest(int[] arr, int target) {
-        Arrays.sort(arr);
-        
-    }
+
+class Solution {
+    public int threeSumClosest(int[] arr, int target) {
+        Arrays.sort(arr);
+        int n = arr.length;
+        
+    }
 }
