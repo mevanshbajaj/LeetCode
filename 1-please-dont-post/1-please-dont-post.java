4913069e3978c4381995a6fb5a8a8f9a0159efa7
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-06 17:28:57 +0530

    solution: 1. Please don't post (java)

--- 1-please-dont-post/1-please-dont-post.java
+++ 1-please-dont-post/1-please-dont-post.java
@@ -1,31 +1,8 @@
 class Solution {
-    public List<List<Integer>> fourSum(int[] nums, int target) {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        int n = nums.length;
-        for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int start = j + 1;
-                int end = n - 1;
-                long newtarget = (long)target - nums[i] - nums[j];
-                while (start < end) {
-                    long currsum = nums[start] + nums[end];
-                    if (currsum < newtarget) {
-                        start++;
-                    } else if (currsum > newtarget) {
-                        end--;
-                    } else {
-                        res.add(Arrays.asList(nums[i], nums[j], nums[start], nums[end]));
-                        while (start < end && nums[start] == nums[start + 1]) start++;
-                        while (start < end && nums[end] == nums[end - 1]) end--;
-                        start++;
-                        end--;
-                    }
-                }
-            }
-        }
-        return res;
+        int start , int end , int currsum , int target;
+        
     }
 }
