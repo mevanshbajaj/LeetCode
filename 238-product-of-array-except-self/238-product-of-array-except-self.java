03064fe6b31d7460207cf67ade57792803dc45f8
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-07 17:19:58 +0530

    solution: 238. Product of Array Except Self (java)

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,5 +1,17 @@
-class Solution {
-    public int[] productExceptSelf(int[] arr) {
-        
-    }
+
+class Solution {
+    public int[] productExceptSelf(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        ans[0] = 1;
+        for(int i=1;i<n;i++){
+            ans[i] = ans[i-1]*arr[i-1];
+        }
+        int suffix = 1;
+        for(int i = n-1;i>=0;i--){
+            ans[i] *= suffix;
+            suffix *= arr[i];
+        }
+        return ans;
+    }
 }
