b53adf7d397025064142b32b4b89d4f453b63534
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-07 17:36:26 +0530

    solution: 146. LRU Cache (java)

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -1,15 +1,14 @@
+class LRUCache {
 
-class LRUCache {
-
-    public LRUCache(int capacity) {
-        
-    }
-    
-    public int get(int key) {
-        
-    }
-    
-    public void put(int key, int value) {
-        
-    }
+    public LRUCache(int capacity) {
+        
+    }
+    
+    public int get(int key) {
+        
+    }
+    
+    public void put(int key, int value) {
+        
+    }
 }
