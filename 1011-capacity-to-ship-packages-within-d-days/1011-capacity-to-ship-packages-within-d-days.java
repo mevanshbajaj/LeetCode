c3185e55d864a44fc71554086e9705aed197aff1
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-07 16:58:34 +0530

    solution: 1011. Capacity To Ship Packages Within D Days (java)

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,6 +1,25 @@
-
-class Solution {
-    public int shipWithinDays(int[] weights, int days) {
-        int le
-    }
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+        int left = 0;
+        int right = 0;
+        for(int x : weights){
+            left = Math.max(left,x);
+            right += x;
+        }
+        while(left <= right){
+            int mid = left + (right-left)/2;
+            int day = 1;
+            int cw = 0;
+            for(int w : weights){
+                if(cw + w > mid){
+                    day++;
+                    cw = w;
+                }
+                else cw += w;
+            }
+            if(day <= days) right = mid -1;
+            else left = mid + 1;
+        }
+        return left;
+    }
 }
