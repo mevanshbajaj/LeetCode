d1454d30a7367b54242eb6a8ef4d62a2c8c87bb2
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-07 09:21:27 +0530

    solution: 1517. Find Users With Valid E-Mails (java)

--- 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.java
+++ 1517-find-users-with-valid-e-mails/1517-find-users-with-valid-e-mails.java
@@ -1,8 +1,4 @@
 
-SELECT *
-FROM Users
-WHERE REGEXP_LIKE(
-    mail,
-    '^[A-Za-z][A-Za-z0-9._-]*@leetcode\\.com$',
-    'c'
-);
+# Write your MySQL query statement below
+select * from users 
+where mail regexp '^[A-Za-z][A-Za-z0-9_.-]*@leetcode\\.com$','c' ;
