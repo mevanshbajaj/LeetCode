cecd8d75ba9ff7ed0f1916dc4b8829a60bb1c233
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-08 16:44:23 +0530

    solution: 20. Valid Parentheses (java)

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,26 +1,25 @@
-
-class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for (char ch : s.toCharArray()) {
-            // Push expected closing bracket
-            if (ch == '(') {
-                st.push(')');
-            } 
-            else if (ch == '[') {
-                st.push(']');
-            } 
-            else if (ch == '{') {
-                st.push('}');
-            } 
-            else {
-                // If stack empty OR mismatch → invalid
-                if (st.isEmpty() || st.pop() != ch) {
-                    return false;
-                }
-            }
-        }
-        // If stack empty → valid
-        return st.isEmpty();
-    }
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char ch : s.toCharArray()) {
+            // Push expected closing bracket
+            if (ch == '(') {
+                st.push(')');
+            } 
+            else if (ch == '[') {
+                st.push(']');
+            } 
+            else if (ch == '{') {
+                st.push('}');
+            } 
+            else {
+                // If stack empty OR mismatch → invalid
+                if (st.isEmpty() || st.pop() != ch) {
+                    return false;
+                }
+            }
+        }
+        // If stack empty → valid
+        return st.isEmpty();
+    }
 }
