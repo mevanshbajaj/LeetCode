3c32ea96e257ece9d45317b642bfe824cd91a232
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-08 17:02:06 +0530

    solution: 496. Next Greater Element I (java)

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,7 +1,16 @@
-
-class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        Stack<Integer> st = new Stack<>();
-        for()
-    }
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer> st = new Stack<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+         for (int num : nums2) {
+            while (!st.isEmpty() && num > st.peek()) {
+                map.put(st.pop(), num); 
+            }
+            st.push(num);
+        }
+        int[] res = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) 
+        res[i] = map.getOrDefault(nums1[i], -1);
+        return res;
+    }
 }
