82070efdebf53ac85c0741891751be89b7f0fbc5
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-08 20:30:07 +0530

    solution: 53. Maximum Subarray (java)

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -1,6 +1,12 @@
 
 class Solution {
-    public int maxSubArray(int[] nums) {
-        
+    public int maxSubArray(int[] arr) {
+     int sum = 0 , max = arr[0];
+     for(int x : arr){
+        sum += x;
+        max = Math.max(max,sum);
+        if(sum<0) sum=0;
+     }
+     return max;
     }
 }
