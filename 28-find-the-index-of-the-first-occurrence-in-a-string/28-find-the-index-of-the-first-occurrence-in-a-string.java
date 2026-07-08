43e0d020d78417ed8221ce765ac7cadfbae5e1dd
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-08 13:00:25 +0530

    solution: 28. Find the Index of the First Occurrence in a String (java)

--- 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,7 +1,10 @@
-class Solution {
-    public int strStr(String s1, String s2) {
-        for(int i=0;i<s1.length();i++){
-         if(s1.charAt(i) == s2.charAt(0))
-        }
-    }
+
+class Solution {
+    public int strStr(String s1, String s2) {
+        for(int i=0;i<s1.length();i++){
+         if(s1.charAt(i) == s2.charAt(0))
+         if(s1.substring(i,s2.length()-1+i).equals(s2)) return i;
+        }
+        return -1;
+    }
 }
