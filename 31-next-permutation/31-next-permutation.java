8707df5820b16986bc5ae615483fcffbbd685911
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-10 16:38:27 +0530

    solution: 31. Next Permutation (java)

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -1,6 +1,10 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
-        
+        int i = n - 2;
+        while(i >= 0 && nums[i] >= nums[i+1]) i--;
+        if(i >= 0){
+            
+        }
     }
 }
