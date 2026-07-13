9bfafd6ffa44bd10a53c568361058637f2f2ab02
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-13 16:25:52 +0530

    solution: 509. Fibonacci Number (java)

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,7 +1,8 @@
-class Solution {
-    public int fib(int n) {
-        if(n == 0) return 0;
-        
-        return f()
-    }
+
+class Solution {
+    public int fib(int n) {
+        if(n == 0) return 0;
+        
+        return f()
+    }
 }
