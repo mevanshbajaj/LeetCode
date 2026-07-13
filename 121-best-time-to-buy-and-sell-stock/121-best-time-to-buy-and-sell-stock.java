8162eba2ad66f74db39c22999b3a0b85b0500777
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-13 14:23:43 +0530

    solution: 121. Best Time to Buy and Sell Stock (java)

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,12 +1,7 @@
 
 class Solution {
     public int maxProfit(int[] arr) {
-        int maxprofit = 0;
-        int minprice = Integer.MAX_VALUE;
-        for(int price : arr){
-            minprice = Math.min(minprice,price);
-            maxprofit = Math.max(maxprofit,price-minprice);
-        }
-        return maxprofit;
+        Stack<Integer> st = new Stack<>();
+        
     }
 }
