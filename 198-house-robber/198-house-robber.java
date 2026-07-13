f63d00fa659bcaf8952a3ad4cb84e8a9f4aa706d
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-13 16:53:38 +0530

    solution: 198. House Robber (java)

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,6 +1,12 @@
-
-class Solution {
-    public int rob(int[] nums) {
-        
-    }
+class Solution {
+    public int rob(int[] nums) {
+        
+    }
+    int fun(int[] arr){
+        if(dp[n] != -1) return dp[n];
+        // take step 
+        int take = arr[i] + fun(i+1,nums);
+        //skip step
+        int skip = 
+    }
 }
