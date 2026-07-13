8cd3377db9577c5f7b74cbf1d5d27061ee4c2c07
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-13 15:50:50 +0530

    solution: 33. Search in Rotated Sorted Array (java)

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -1,18 +1,16 @@
 class Solution {
     public int search(int[] arr, int target) {
-        int left = 0 , right = arr.length-1;
+        int left = 0; int right = arr.length-1;
         while(left<=right){
             int mid = left + (right-left)/2;
             if(arr[mid] == target) return mid;
-            // check left and right half differently
             if(arr[left] <= arr[mid]){
-                if(arr[left] <= target && target < arr[mid]) right = mid-1;
-                else left = mid+1;
+                if(target >= arr[left] && target < arr[mid]) right = mid - 1;
+                else left = mid + 1;
             }
-            //right 
             else{
-                if(arr[mid] < target && target <= arr[right]) left = mid + 1;
-                else right = mid-1;
+                if(arr[mid] < target && target <= arr[right]) left = mid+1;
+                else right = mid - 1;
             }
         }
         return -1;
