e5d1b08965be4fe3ece8a2b878a920fe2d85562f
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-14 16:59:50 +0530

    solution: 278. First Bad Version (java)

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -1,8 +1,9 @@
-/* The isBadVersion API is defined in the parent class VersionControl.
-      boolean isBadVersion(int version); */
 
-public class Solution extends VersionControl {
-    public int firstBadVersion(int n) {
-        
-    }
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        
+    }
 }
