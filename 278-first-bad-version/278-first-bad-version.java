2730e3872e1e9346374029a44acc512c7a017ac4
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-14 17:04:43 +0530

    solution: 278. First Bad Version (java)

--- 278-first-bad-version/278-first-bad-version.java
+++ 278-first-bad-version/278-first-bad-version.java
@@ -3,8 +3,8 @@
 
 public class Solution extends VersionControl {
     public int firstBadVersion(int n) {
-        int start = 0 , end = n;
-        while(start<=end){
+        int start = 1 , end = n;
+        while(start<end){
         int mid = start + (end-start)/2;
         if(isBadVersion(mid)) end = mid;
         else start = mid + 1;
