dd20d970cec597a521ff1d0c89f06daf1d46dfe7
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-14 16:41:40 +0530

    solution: 189. Rotate Array (java)

--- 189-rotate-array/189-rotate-array.java
+++ 189-rotate-array/189-rotate-array.java
@@ -3,10 +3,13 @@ class Solution {
         int n = arr.length;
         k=k%n;
         rev(arr,0,n-k-1);
-        rev(arr,0,n-k-1);
-        rev(arr,0,n-k-1);
+        rev(arr,n-k,n-1);
+        rev(arr,0,n-1);
     }
     void rev(int[] arr , int start , int end){
-
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;end--;
     }
 }
