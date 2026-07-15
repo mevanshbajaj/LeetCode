2a037da170310dc5a0271f30c12b17ae639fd619
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-15 16:42:33 +0530

    solution: 72. Edit Distance (java)

---
+++ 72-edit-distance/72-edit-distance.java
@@ -0,0 +1,22 @@
+
+class Solution {
+    int[][] dp;
+    public int minDistance(String word1, String word2) {
+        int n = word1.length();
+        int m = word2.length();
+        dp = new int[n][m];
+        for(int[] x : dp) Arrays.fill(x , -1);
+        return helper(word1,word2,n-1,m-1);
+    }
+    public int helper(String s1 , String s2 , int i, int j){
+        if(i < 0 ) return j+1;
+        if(j < 0) return i+1;
+        if (dp[i][j] != -1) return dp[i][j];
+        if(s1.charAt(i) == s2.charAt(j))
+        return dp[i][j] = helper(s1,s2,i-1,j-1);
+        int insert = helper(s1, s2, i, j - 1);
+        int delete = helper(s1, s2, i - 1, j);
+        int replace =helper(s1, s2, i - 1, j - 1);
+        return dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+    }
+    } 
