5c9791b472261109115f61f370ac3b74c7a3bac3
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-17 17:21:40 +0530

    solution: 658. Find K Closest Elements (java)

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,5 +1,6 @@
-class Solution {
-    public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        
-    }
+
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+        
+    }
 }
