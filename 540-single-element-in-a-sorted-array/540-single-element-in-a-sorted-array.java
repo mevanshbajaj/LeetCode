dae61c35c0b5a10361fd154f918d6de523460887
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-17 17:30:54 +0530

    solution: 540. Single Element in a Sorted Array (java)

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,13 +1,12 @@
-
-class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        if(n == 1 || nums[0] != nums[1]) return nums[0];
-        for(int i=1;i<n-2;i++){
-            if(nums[i] != nums[i+1] && nums[i] != nums[i-1])
-            return nums[i];
-        }
-            if(nums[n-1] != nums[n-2]) return nums[n-1];
-        return -1;
-    }
+class Solution {
+    public int singleNonDuplicate(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i : arr){
+            if(!set.contains(i)) { 
+            set.add(i);
+            }
+            else set.remove(i);
+        }
+        return set.iterator().next();
+    }
 }
