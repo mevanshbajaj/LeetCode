2924c706a64cbadd1a8a62a6673002b64d5840ee
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-17 11:06:17 +0530

    solution: 1768. Merge Strings Alternately (java)

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,5 +1,20 @@
-class Solution {
-    public String mergeAlternately(String word1, String word2) {
-        
-    }
+
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+        int a = word1.length();
+        int b = word2.length();
+        int i = 0, j = 0;
+        StringBuilder ans = new StringBuilder();
+        while (i < a && j < b) {
+            ans.append(word1.charAt(i++));
+            ans.append(word2.charAt(j++));
+        }
+        while (i < a) {
+            ans.append(word1.charAt(i++));
+        }
+        while (j < b) {
+            ans.append(word2.charAt(j++));
+        }
+        return ans.toString();
+    }
 }
