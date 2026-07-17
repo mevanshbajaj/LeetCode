578eaece43b8e5ecd1dcd59c0e17d81c63a46c42
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-17 16:43:04 +0530

    solution: 1588. Sum of All Odd Length Subarrays (java)

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,6 +1,12 @@
-
-class Solution {
-    public int sumOddLengthSubarrays(int[] arr) {
-        
-    }
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int res = 0;
+        int n = arr.length;
+        for(int i=0;i<n;i++){
+            int total = (i-1) * (n-1);
+            int oddcount = (total + 1) / 2;
+            res += oddcount * arr[i];
+        }
+        return res;
+    }
 }
