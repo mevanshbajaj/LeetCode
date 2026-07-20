18e63fc7b42c1aa01f4eb3d8c500533d82f29f85
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-20 17:00:42 +0530

    solution: 199. Binary Tree Right Side View (java)

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.java
@@ -1,23 +1,35 @@
-
-// using level order 
-class Solution {
-    public List<Integer> rightSideView(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if(root==null) return result;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
-        while(!q.isEmpty()){
-            int size = q.size();
-            int store = 0; // store last element
-            for(int i=0;i<size;i++){
-                TreeNode a = q.poll();
-                store = a.val;
-                if(a.left!=null) q.add(a.left);
-                if(a.right!=null) q.add(a.right);
-            }
-            result.add(store);
-        }
-        return result;
-
-    }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if(root==null) return result;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            int size = q.size();
+            int store = 0;
+            for(int i=0;i<size;i++){
+                TreeNode a = q.poll();
+                store = a.val;
+                if(a.left!=null) q.add(a.left);
+                if(a.right!=null) q.add(a.right);
+                result.add(store);
+            }
+        }
+        return result;
+    }
 }
