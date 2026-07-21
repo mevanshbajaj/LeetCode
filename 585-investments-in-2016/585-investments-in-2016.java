d6220d7faf8085a5cbfbda2c8715ec7385c77d97
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-21 14:21:48 +0530

    solution: 585. Investments in 2016 (java)

---
+++ 585-investments-in-2016/585-investments-in-2016.java
@@ -0,0 +1,20 @@
+
+# Write your MySQL query statement below
+SELECT
+    ROUND(SUM(i.tiv_2016), 2) AS tiv_2016
+FROM Insurance i
+JOIN (
+    SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015
+    HAVING COUNT(*) > 1
+) t
+ON i.tiv_2015 = t.tiv_2015
+JOIN (
+    SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*) = 1
+) l
+ON i.lat = l.lat
+AND i.lon = l.lon;
