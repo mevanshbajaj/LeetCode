766d2e1befa706537c14eab2593e8e7cf7171e69
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-21 16:43:57 +0530

    solution: 1008. Construct Binary Search Tree from Preorder Traversal (java)

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.java
@@ -1,5 +1,21 @@
-class Solution {
-    public TreeNode bstFromPreorder(int[] preorder) {
-        
-    }
+
+class Solution {
+    public TreeNode helper
+    (int[] preorder , int[] inorder , int prelow , int prehigh , int inlow ,int inhigh){
+        if(prelow > prehigh || inlow > inhigh) return null;
+        TreeNode root = new TreeNode(preorder[prelow]);
+        int r = 0;
+        while(inorder[r]!=preorder[prelow]) r++;
+        int leftsize = r - inlow;
+        root.left = helper(preorder,inorder,prelow+1,prelow+leftsize,inlow,r-1);
+        root.right = helper(preorder,inorder,prelow+leftsize+1,prehigh+leftsize,r+1,inhigh);
+        return root;
+    }
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int n = preorder.length;
+        int[] inorder = Arrays.copyOf(preorder,n);
+        Arrays.sort(inorder);
+        return helper(preorder,inorder,0,n-1,0,n-1);
+
+    }
 }
