2e32ff4a303342710cb2728bbe73be790de2aa20
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-21 16:59:03 +0530

    solution: 437. Path Sum III (java)

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -1,25 +1,28 @@
-
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int helper(TreeNode root , int sum){
-
-    }
-    public int pathSum(TreeNode root, int targetSum) {
-        if(root==null) return 0;
-        return helper(root,sum) + pathSum(root.left,sum) + 
-    }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int helper(TreeNode root , int sum){
+        if(root==null) return 0;
+        int count = 0;
+         if(root.val==sum) count++;
+         count += helper(root.left,sum-root.val) + helper(root.right,sum-root.val);
+        return count;
+    }
+    public int pathSum(TreeNode root, int targetSum) {
+        if(root==null) return 0;
+        return helper(root,sum) + pathSum(root.left,sum) + pathSum(root.right,sum);
+    }
 }
