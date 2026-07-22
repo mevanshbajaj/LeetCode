6870854075353770de421e1613cdd07de4d404b2
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-22 17:04:54 +0530

    solution: 437. Path Sum III (java)

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -14,9 +14,15 @@
  * }
  */
 class Solution {
-
-    public int pathSum(TreeNode root, int targetSum) {
+    int dfs(TreeNode root , int sum){
+        if(root==null) return 0;
+        int count = 0;
+        if(root.val==sum) count++;
+        count += dfs(root.left,root.val-sum) + dfs(root.right,root.val-sum);
+        return count;
+    }
+    public int pathSum(TreeNode root, int sum) {
         if(root == null) return 0;
-        
+        return dfs(root.left,sum) + dfs(root.right,sum) + pathSum(root,sum);
     }
 }
