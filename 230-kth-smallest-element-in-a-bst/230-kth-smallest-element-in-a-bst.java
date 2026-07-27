06a96b4c04a225b244b1da5793964bceecafbb7e
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-27 15:04:15 +0530

    solution: 230. Kth Smallest Element in a BST (java)

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -1,21 +1,29 @@
-
 /**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        
-    }
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+        int count = 0;
+    public int kthSmallest(TreeNode root, int k) {
+        return inorder(root);
+    }
+    int inorder(TreeNode root){
+        if(root == null) return -1;
+        int left = inorder(root.left);
+        if(left != -1) return left;
+        count++;
+        if(count == k) return root.val;
+        return inorder(root.right);
+    }
 }
