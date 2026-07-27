724f7d0f4aca2444947227014b245b21931b2fa5
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-27 15:37:52 +0530

    solution: 450. Delete Node in a BST (java)

--- 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
+++ 450-delete-node-in-a-bst/450-delete-node-in-a-bst.java
@@ -19,8 +19,17 @@ class Solution {
         return root;
     }
     public TreeNode deleteNode(TreeNode root, int key) {
-        if(root.left == null) return root.right;
-        if(root.right == null) return root.left;
-        
+        if(root==null) return null;
+        if(root.val > key) root.left = deleteNode(root.left,key);
+        else if(root.val < key) root.right = deleteNode(root.right,key);
+        else{
+            if(root.left == null) return root.right;
+            if(root.right == null) return root.left;
+            TreeNode small = findmin(root.right);
+            root.val = small.val;
+            root.right=deleteNode(root.right,small.val);
+        }
+        return root;
+    }
     }
 }
