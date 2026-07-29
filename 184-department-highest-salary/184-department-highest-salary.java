454bd85f729a5aa0b3109281332fd4b1b0c5c6a9
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-29 21:25:18 +0530

    solution: 184. Department Highest Salary (java)

---
+++ 184-department-highest-salary/184-department-highest-salary.java
@@ -0,0 +1,8 @@
+
+# Write your MySQL query statement below
+select d.name as Department , e.name as Employee , e.salary as Salary from Employee e
+join department d on e.departmentId = d.id
+where (e.departmentId , e.salary) in
+(select departmentId  , max(salary) from
+employee group by departmentId
+)
