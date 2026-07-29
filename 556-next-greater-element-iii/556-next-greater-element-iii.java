70ea815fe350c0d48e7809f4ea4a52a23bf88fd7
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-07-29 14:00:21 +0530

    solution: 556. Next Greater Element III (java)

---
+++ 556-next-greater-element-iii/556-next-greater-element-iii.java
@@ -0,0 +1,20 @@
+
+class Solution {
+    public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        Stack<Integer> st = new Stack<>();
+        Arrays.fill(res, -1);
+        for (int i = 2 * n - 1; i >= 0; i--) {
+            int num = nums[i % n];
+            while (!st.isEmpty() && st.peek() <= num) {
+                st.pop();
+            }
+            if (i < n) {  // only fill result in first pass
+                if (!st.isEmpty()) res[i] = st.peek();
+            }
+            st.push(num);
+        }
+        return res;
+    }
+}
