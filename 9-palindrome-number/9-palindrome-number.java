b6adb31451c82d9bb3f8440d85b0f826a0f306a6
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-06 20:11:26 +0530

    solution: 9. Palindrome Number (java)

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,6 +1,15 @@
-
-class Solution {
-    public boolean isPalindrome(int x) {
-        
-    }
+class Solution {
+    public boolean isPalindrome(int x) {
+    if(x<0) return false;
+    int n=x;
+    int m=0;
+    int rem=0;
+    while(x>0){
+        rem=x%10;
+        m=m*10+rem;
+        x=x/10;
+    }
+    if(n==m) return true;
+    return false;
+    }
 }
