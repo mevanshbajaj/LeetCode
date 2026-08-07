b673d2fc9b581bceeccd49fc0bfc004ab4f5002d
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-07 18:41:07 +0530

    solution: 1. Two Sum (java)

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,9 +1,13 @@
-class Solution {
-    public int[] twoSum(int[] arr, int target) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<=n;i++){
-            int rem = target - arr[i];
-            
-        }
-    }
+
+class Solution {
+    public int[] twoSum(int[] arr, int target) {
+        int n 
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<=n;i++){
+            int rem = target - arr[i];
+            if(map.contains(rem)) return new int[] {map.get(rem),i};
+            else map.put(arr[i] , i);
+        }
+        return new int[]{};
+    }
 }
