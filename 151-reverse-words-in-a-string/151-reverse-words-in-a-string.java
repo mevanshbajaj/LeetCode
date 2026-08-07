c9c7ca7ad37d0ef6f810151c352f09b09fe03ca7
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-07 18:43:17 +0530

    solution: 151. Reverse Words in a String (java)

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.java
@@ -1,5 +1,11 @@
-class Solution {
-    public String reverseWords(String s) {
-        
-    }
+
+class Solution {
+    public String reverseWords(String s) {
+      String[] word = s.trim().split("\\s+");
+      StringBuilder sb = new StringBuilder();
+      for(int i = word.length-1;i>=0;i--){
+        sb.append(word[i]).append(" ");
+      }
+      return sb.toString().trim();
+    }
 }
