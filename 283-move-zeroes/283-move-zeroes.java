13148e71f794110ee60e966d5700d71b38bbfeb3
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-12 22:17:01 +0530

    solution: 283. Move Zeroes (java)

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,8 +1,8 @@
-class Solution {
-    public void moveZeroes(int[] arr) {
-        int idx = 0;
-        for(int x : arr){
-            if(x != 0) 
-        }
-    }
+
+class Solution {
+    public void moveZeroes(int[] arr) {
+        int idx = 0;
+        for(int x : arr) if(x != 0) arr[idx++] = x;
+       while(idx < arr.length) arr[idx++] = 0;
+    }
 }
