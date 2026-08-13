bb0f5fc1fabd51b7afc595ab9124ce9a7697043b
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-13 17:28:41 +0530

    solution: 2760. Longest Even Odd Subarray With Threshold (java)

--- 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
+++ 2760-longest-even-odd-subarray-with-threshold/2760-longest-even-odd-subarray-with-threshold.java
@@ -1,16 +1,14 @@
 
 class Solution {
     public int longestAlternatingSubarray(int[] arr, int threshold) {
-        int len = 0;
+        int len = 0 , n = arr.length;
         int start = 0;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < n; i++) {
             if (arr[i] > threshold || arr[i] % 2 != 0) {
-                start = i + 1;
-                continue;
+                start = i + 1; continue;
             }
             int j = i + 1;
-            while (j < arr.length &&
-                   arr[j] <= threshold && arr[j] % 2 != arr[j - 1] % 2) j++;
+            while (j < n && arr[j] <= threshold && arr[j] % 2 != arr[j - 1] % 2) j++;
             len = Math.max(len, j - i);
             i = j - 1;
         }
