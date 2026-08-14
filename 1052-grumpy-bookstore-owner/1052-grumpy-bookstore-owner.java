48ffbd6a60a52884719f0dcdff735cc6ae909503
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-14 18:10:40 +0530

    solution: 1052. Grumpy Bookstore Owner (java)

---
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int maxSatisfied(int[] customers, int[] arr, int k) {
+        int n = customers.length, i = 0, j = k - 1;
+        int unsatisfied = 0;
+        int satisfied = 0;
+
+        int a = i, b = j, maxunsatisfied = 0;
+
+        // Customers already satisfied
+        for (int x = 0; x < n; x++) {
+            if (arr[x] == 0) {
+                satisfied += customers[x];
+            }
+        }
+
+        // First window
+        for (int x = i; x <= j; x++) {
+            if (arr[x] == 1) {
+                unsatisfied += customers[x];
+            }
+        }
+
+        // Sliding window
+        while (j < n) {
+
+            if (maxunsatisfied < unsatisfied) {
+                maxunsatisfied = unsatisfied;
+                a = i;
+                b = j;
+            }
+            i++;
+            j++;
+            if (j < n && arr[j] == 1) {
+                unsatisfied += customers[j];
+            }
+            if (arr[i - 1] == 1) {
+                unsatisfied -= customers[i - 1];
+            }
+        }
+        return satisfied + maxunsatisfied;
+    }
+}
