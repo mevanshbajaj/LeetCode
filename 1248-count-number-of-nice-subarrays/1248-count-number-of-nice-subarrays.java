0e9ac47cbbc8b1068040cc4418e42394b86458e5
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-16 17:18:38 +0530

    solution: 1248. Count Number of Nice Subarrays (java)

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,6 +1,30 @@
-
-class Solution {
-    public int numberOfSubarrays(int[] nums, int k) {
-        
-    }
+class Solution {
+    public int numberOfSubarrays(int[] arr, int k) {
+        int n = arr.length;
+        int i =0 , j = 0 , a = 0 , b = 0 , count = 0 , k2 = 0;
+        // for(int x=0;x<n;x++) arr[x] %= 2;
+        // move  ito first odd
+        while(i<n&&arr[i]%2=0) i++;
+        // j ko kth odd number
+        while(j<n&&k2 < k){
+            if(arr[j]%2!=0){
+                j++;k2++;
+            }
+            j--;
+        }
+        b = j + 1;
+        while(b<n&&arr[b]%2==0) b++;
+        b--;
+        while(b<n){
+            count += (i-a+1)*(b-j+1);
+            a = i+1;
+            i++;
+            while(i<n&&arr[i]%2==0) i++;
+            j = b+1;
+            b=j+1;
+            while(b<n&&arr[b]%2==0) b++;
+            b--;
+        }
+        return count;
+    }
 }
