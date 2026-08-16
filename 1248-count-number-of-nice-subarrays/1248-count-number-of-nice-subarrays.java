611c4ec6a60edffbc0c2a5a4f6e9b9026d54b878
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-16 17:23:09 +0530

    solution: 1248. Count Number of Nice Subarrays (java)

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.java
@@ -1,28 +1,45 @@
 class Solution {
     public int numberOfSubarrays(int[] arr, int k) {
         int n = arr.length;
-        int i =0 , j = 0 , a = 0 , b = 0 , count = 0 , k2 = 0;
-        // for(int x=0;x<n;x++) arr[x] %= 2;
-        // move  ito first odd
-        while(i<n&&arr[i]%2==0) i++;
-        // j ko kth odd number pe leke jana
-        while(j<n&&k2 < k){
-            if(arr[j]%2!=0){
-                j++;k2++;
+        int i = 0, j = 0, a = 0, b = 0, count = 0, k2 = 0;
+
+        // Move i to first odd
+        while (i < n && arr[i] % 2 == 0)
+            i++;
+
+        // Move j to the kth odd
+        while (j < n && k2 < k) {
+            if (arr[j] % 2 != 0) {
+                j++;
+                k2++;
             }
         }
+
+        // Fewer than k odd numbers
+        if (k2 < k)
+            return 0;
         j--;
+        // Move b to the last even after kth odd
         b = j + 1;
-        while(b<n&&arr[b]%2==0) b++;
+        while (b < n && arr[b] % 2 == 0)
+            b++;
         b--;
-        while(b<n){
-            count += (i-a+1)*(b-j+1);
-            a = i+1;
+        while (b < n) {
+            count += (i - a + 1) * (b - j + 1);
+            // Move to next odd
+            a = i + 1;
             i++;
-            while(i<n&&arr[i]%2==0) i++;
-            j = b+1;
-            b=j+1;
-            while(b<n&&arr[b]%2==0) b++;
+
+            while (i < n && arr[i] % 2 == 0)
+                i++;
+
+            // Move j to next odd
+            j = b + 1;
+
+            b = j + 1;
+            while (b < n && arr[b] % 2 == 0)
+                b++;
+
             b--;
         }
         return count;
