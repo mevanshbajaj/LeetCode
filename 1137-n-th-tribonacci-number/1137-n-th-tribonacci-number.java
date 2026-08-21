19d9fb814bd11e052f6aceb200fb4ef6026d1a5e
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-21 16:23:36 +0530

    solution: 1137. N-th Tribonacci Number (java)

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,16 +1,17 @@
-class Solution {
-    public int helper(int n , int[] dp){
-        if(n <= 1) return dp[n];
-        if(n == 2) return 1;
-        if(dp[n] != -1) return dp[n];
-        return dp[n] = helper(n-1,dp) + helper(n-2,dp) + helper(n-3,dp);
-    }
-    public int tribonacci(int n) {
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        dp[0] = 0;
-        if (n >= 1) dp[1] = 1;
-        if (n >= 2) dp[2] = 1;
-        return helper(n ,dp);
-    }
+
+class Solution {
+    public int helper(int n , int[] dp){
+        if(n <= 1) return dp[n];
+        if(n == 2) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = helper(n-1,dp) + helper(n-2,dp) + helper(n-3,dp);
+    }
+    public int tribonacci(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        if (n >= 1) dp[1] = 1;
+        if (n >= 2) dp[2] = 1;
+        return helper(n ,dp);
+    }
 }
