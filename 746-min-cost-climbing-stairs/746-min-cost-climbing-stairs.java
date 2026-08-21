51b9d5c4d1e2c69760340586c6cc4edb098deeb0
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-21 16:10:33 +0530

    solution: 746. Min Cost Climbing Stairs (java)

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,13 +1,15 @@
 
 class Solution {
-    public int mincost(int[] arr,int n) {
+    public int mincost(int[] arr,int n , int[] dp) {
         // here n is position where person is standing 
         if(n == 0 || n == 1) return arr[n];
-        
-        return arr[n] + Math.min(mincost(arr,n-1),mincost(arr,n-2));
+        if(dp[n] != 0) return dp[n];
+        return dp[n]=arr[n] + Math.min(mincost(arr,n-1),mincost(arr,n-2));
     }
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
-        return Math.min(mincost(cost,n-1),mincost(cost,n-2));
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return Math.min(mincost(cost,n-1,dp),mincost(cost,n-2,dp));
     }
 }
