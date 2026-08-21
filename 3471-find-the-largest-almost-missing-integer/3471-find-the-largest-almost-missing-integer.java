7f1b962eebf8c82ad48ad07790f19bb69666c67e
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-21 11:10:15 +0530

    solution: 3471. Find the Largest Almost Missing Integer (java)

--- 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.java
@@ -1,22 +1,21 @@
-
-class Solution {
-    public int largestInteger(int[] arr, int k) {
-        int n = arr.length;
-        int ans = -1;
-        for(int x : arr){
-            int count = 0;
-            for(int i=0;i<=n-k;i++){
-                boolean flag = false;
-                for(int j=i;j<i+k;j++){
-                    if(arr[j] == x){
-                        flag = true;
-                        break;
-                    }
-                }
-                if(flag) count++;
-            }
-            if(count == 1) ans = Math.max(ans,x);
-        }
-        return ans;
-    }
+class Solution {
+    public int largestInteger(int[] arr, int k) {
+        int n = arr.length;
+        int ans = -1;
+        for(int x : arr){
+            int count = 0;
+            for(int i=0;i<=n-k;i++){
+                boolean flag = false;
+                for(int j=i;j<i+k;j++){
+                    if(arr[j] == x){
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag) count++;
+            }
+            if(count == 1) ans = Math.max(ans,x);
+        }
+        return ans;
+    }
 }
