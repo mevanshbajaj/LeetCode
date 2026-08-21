e2f3b4bf5a14453dbd179b675d81abe1da85b64b
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-21 18:03:23 +0530

    solution: 198. House Robber (java)

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,19 +1,19 @@
-class Solution {
-    public int amount(int[] arr , int i){
-        int n = arr.length;
-        int[] dp = new int[n];
-        if(i>=n) return 0;
-        if(dp[n] != -1) return dp[i];
-        int take = arr[i] + amount(arr,i+2);
-        int skip = amount(arr,i+1);
-        dp[n] = Math.max(take,skip);
-        return dp[n];
-    }
-    public int rob(int[] arr) {
-        int n = arr.length;
-        // i varies from 0 - n-1
-        int[] dp = new int[n];
-        Arrays.fill(dp,-1);
-        return amount(arr,0);
-    }
+
+class Solution {
+    public int amount(int[] arr , int i , int[] dp){
+        int n = arr.length;
+        if(i>=n) return 0;
+        if(dp[i] != -1) return dp[i];
+        int take = arr[i] + amount(arr,i+2,dp);
+        int skip = amount(arr,i+1,dp);
+        dp[i] = Math.max(take,skip);
+        return dp[i];
+    }
+    public int rob(int[] arr) {
+        int n = arr.length;
+        // i varies from 0 - n-1
+        int[] dp = new int[n];
+        Arrays.fill(dp,-1);
+        return amount(arr,0);
+    }
 }
