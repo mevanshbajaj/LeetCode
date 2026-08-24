66a645b04223efa876e267200120619a4ba55c94
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-08-24 21:14:19 +0530

    solution: 62. Unique Paths (java)

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,13 @@
 
-class Solution {
-    public int paths(int i , int j , int m , int n,int[][] dp) {
-        if(i >= m || j >= n) return 0;
-        if(i == m-1 || j == n-1) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-        int rightways = paths(i,j+1,m,n,dp);
-        int downways = paths(i+1,j,m,n,dp);
-        return dp[i][j] = rightways + downways;
-    }
+class Solution { //tabulation
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for (int[] row : dp)
-        Arrays.fill(row,-1);
-        return paths(0,0,m,n,dp);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
+        return dp[m][n];
     }
 }
