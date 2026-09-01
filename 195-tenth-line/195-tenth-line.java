d9dbba6a3ac0038047dd5bbfad84bf72012f8a43
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-09-01 15:12:45 +0530

    solution: 195. Tenth Line (java)

--- 195-tenth-line/195-tenth-line.java
+++ 195-tenth-line/195-tenth-line.java
@@ -1,3 +1,11 @@
 
-# Read from the file file.txt and output the tenth line to stdout.
-sed -n '10p' file.txt
+# Read from the file file.txt and print its transposed content to stdout.
+awk '{
+    for (i = 1; i <= NF; i++) {
+        a[i] = (a[i] ? a[i] " " : "") $i
+    }
+}
+END {
+    for (i = 1; i <= NF; i++)
+        print a[i]
+}' file.txt
