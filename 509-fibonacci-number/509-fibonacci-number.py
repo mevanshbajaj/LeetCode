e415a3580a11fd0c2f710e9116bb3f2e35ef9f89
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-09-02 19:04:58 +0530

    solution: 509. Fibonacci Number (python3)

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,7 +1,6 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        a , b = 0,1
-        for _ in range(n):
-            a, b = b, a + b
 
-        return a
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<2:
+            return n
+        return fib(n-1) + fib(n-2)
