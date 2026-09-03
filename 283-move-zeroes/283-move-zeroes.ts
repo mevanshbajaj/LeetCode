06d77a322b4d0dce6bd18679b4a9d3c3e45653e8
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-09-03 12:09:01 +0530

    solution: 283. Move Zeroes (typescript)

--- 283-move-zeroes/283-move-zeroes.ts
+++ 283-move-zeroes/283-move-zeroes.ts
@@ -2,6 +2,10 @@
 /**
  Do not return anything, modify nums in-place instead.
  */
-function moveZeroes(nums: number[]): void {
-    
+function moveZeroes(arr: number[]): void {
+    let idx = 0;
+    for(const x of arr) {
+        if(x != 0) arr[idx++] = x;
+    }
+    while(idx < arr.length) arr[idx++] = 0;
 };
