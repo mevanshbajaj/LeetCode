aa8c1745b884da3014c78249c84967729eb6f45e
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-09-14 18:16:01 +0530

    solution: 836. Rectangle Overlap (java)

--- 836-rectangle-overlap/836-rectangle-overlap.java
+++ 836-rectangle-overlap/836-rectangle-overlap.java
@@ -1,15 +1,8 @@
 
 class Solution {
     public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
-        int x1 = rec1[0];
-        int y1 = rec1[1];
-        int x2 = rec1[2];
-        int y2 = rec1[3];
-        int x3 = rec2[0];
-        int y3 = rec2[1];
-        int x4 = rec2[2];
-        int y4 = rec2[3];
-        if(x4 <= x1 || x2 <= x3 || y2 <= y3 || y4 <= y1) return false;
-        return true;
+        boolean one = rec1[0] < rec2[2] && rec2[0] < rec1[2];
+        boolean two = rec1[1] < rec2[3] && rec2[1] < rec1[3];
+        return one&two;
     }
 }
