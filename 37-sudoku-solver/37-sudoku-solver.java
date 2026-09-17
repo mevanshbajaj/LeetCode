aa4261f5f4a52b5564b04593e0bbfa5b1bff26c8
mevanshbajaj <157448877+mevanshbajaj@users.noreply.github.com>
2026-09-17 18:18:27 +0530

    solution: 37. Sudoku Solver (java)

--- 37-sudoku-solver/37-sudoku-solver.java
+++ 37-sudoku-solver/37-sudoku-solver.java
@@ -1,6 +1,17 @@
 
 class Solution {
     public void solveSudoku(char[][] board) {
-        
+        return solve(row,board,n);
+        if(row == n){
+            c++;
+            return;
+        }
+        for(itnt col=0;col<n;col++){
+            if(issafe(row,col,board)){
+                board[row][col]='Q';
+                solve(row+1,board,n);
+                board[row][col]=' ';
+            }
+        }
     }
 }
